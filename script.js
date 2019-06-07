48d0c515f2186e64a9114fab819a6f8c86ccf8bf
roman1313 <roman@jyaasa.com>
2019-06-07 16:42:42 +0545

    state is now persistent

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function dragMouseDown(e) {
     //mouse position at startup
     pos3 = e.clientX;
     pos4 = e.clientY;
+
     document.onmouseup = closeDragElement;
     // call function when mouse moves
     document.onmousemove = elementDrag;
@@ -36,7 +37,6 @@ function closeDragElement() {
 $("#newWindowCreate").addEventListener('click', function () {
     lastWindow = objects.reverse()[0];
     wid = new ourWindow();
-    console.log(lastWindow);
     wid.id = parseInt(lastWindow.id) + 1;
     wid.top = parseInt(lastWindow.top) + 10 + "px";
     wid.left = parseInt(lastWindow.left) + 10 + "px";
@@ -45,8 +45,7 @@ $("#newWindowCreate").addEventListener('click', function () {
 })
 function bootLoader() {
     var collection = JSON.parse(localStorage.getItem('windows'));
-
-    if (collection == null || localStorage.getItem('windows') == "[{}]") {
+    if (collection == null || localStorage.getItem('windows') == "[{}]" || localStorage.getItem('windows') == "[]") {
         myWindow = new ourWindow();
         objects.push(myWindow);
         myWindow._render();
@@ -58,8 +57,23 @@ function bootLoader() {
             myWindow._render();
         })
     }
+
+    // if (!!collection) {
+    //     collection.forEach(function (propertyCollection) {
+    //         myWindow = new ourWindow(propertyCollection);
+    //         objects.push(myWindow);
+    //         myWindow._render();
+    //     })
+    // }
+    // else {
+    //     myWindow = new ourWindow();
+    //     alert('hello');
+    //     objects.push(myWindow);
+    //     myWindow._render();
+    // }
 }
 
+bootLoader();
 
 function save() {
     var collection = [];
@@ -73,6 +87,7 @@ function save() {
             zIndex: windowObject.zIndex,
             state: windowObject.state
         })
+
     })
     localStorage.setItem('windows', JSON.stringify(collection))
 }
@@ -84,6 +99,8 @@ var left = 200;
 function ourWindow(propertyCollection) {
     var orginalWindow = $('#originalWindow');
     this.newWindowMarkup = orginalWindow.cloneNode(true);
+    this.newWindowMarkup.classList.remove('hidden');
+    console.log(this);
     this.objectId = "window-1";
     this.default = {
         id: 0,
@@ -106,30 +123,55 @@ function ourWindow(propertyCollection) {
     this.zIndex = propertyCollection.zIndex;
     this.state = propertyCollection.state;
 
+    var originalState = [{}];
+
     this.minimize = function () {
-        console.log(this.height);
-        Object.assign(this.newWindowMarkup.style, {
-            top: "93vh",
-            left: "3rem",
-            height: "2.5rem",
-            width: "10rem"
-        })
+        // originalState.push({ id: this.id, left: this.left, top: this.top, height: this.height, width: this.width })
+        this.newWindowMarkup.classList.remove('maximized');
+        this.newWindowMarkup.classList.add('minimized');
+        // Object.assign(this.newWindowMarkup.style, {
+        //     top: "93vh",
+        //     left: "3rem",
+        //     height: "2.5rem",
+        //     width: "10rem"
+        // });
+        this.height = getComputedStyle(this.newWindowMarkup).height;
+        this.width = getComputedStyle(this.newWindowMarkup).width;
+        this.left = getComputedStyle(this.newWindowMarkup).left;
+        this.top = getComputedStyle(this.newWindowMarkup).top;
+
+        // this.newWindowMarkup.classList.add("minimized");
     }
     this.maximize = function () {
-        Object.assign(this.newWindowMarkup.style, {
-            height: "90vh",
-            width: "99vw",
-            top: 0,
-            left: 0
-        })
+
+        // Object.assign(this.newWindowMarkup.style, {
+        //     height: "90vh",
+        //     width: "99vw",
+        //     top: 0,
+        //     left: 0
+        // })
+        this.newWindowMarkup.classList.remove('minimized');
+        this.newWindowMarkup.classList.add('maximized');
+        this.height = getComputedStyle(this.newWindowMarkup).height;
+        this.width = getComputedStyle(this.newWindowMarkup).width;
+        this.left = getComputedStyle(this.newWindowMarkup).left;
+        this.top = getComputedStyle(this.newWindowMarkup).top;
     }
     this.restore = function () {
-        Object.assign(this.newWindowMarkup.style, {
-            height: this.height,
-            width: this.width
-        })
+        this.newWindowMarkup.classList.remove("maximized");
+        this.newWindowMarkup.classList.remove("minimized");
+        this.height = getComputedStyle(this.newWindowMarkup).height;
+        this.width = getComputedStyle(this.newWindowMarkup).width;
+        this.left = getComputedStyle(this.newWindowMarkup).left;
+        this.top = getComputedStyle(this.newWindowMarkup).top;
     }
     this.close = function () {
+        var newThis = this;
+        this.newWindowMarkup.remove();
+        objects = objects.filter(function (item) {
+            return (item.id != newThis.id)
+        })
+        delete (this);
         this.newWindowMarkup.style.display = "none";
     }
 
@@ -143,29 +185,50 @@ function ourWindow(propertyCollection) {
             left: this.left,
             display: 'block'
         })
-        // this.newWindowMarkup.style.height = this.height;
-        // this.newWindowMarkup.style.width = this.width;
-        // this.newWindowMarkup.style.top = this.top;
-        // this.newWindowMarkup.style.left = this.left;
-        // this.newWindowMarkup.style.display = 'block';
-
 
         this.newWindowMarkup.querySelector('.fa-window-minimize').addEventListener('click', function () {
+            console.log(thisWindow);
             handlers.minimizeHandler(thisWindow);
+            console.log(thisWindow);
             console.log(thisWindow.state);
         });
         this.newWindowMarkup.querySelector('.fa-window-maximize').addEventListener('click', function () {
             handlers.maximizeHandler(thisWindow);
+            console.log(thisWindow.state);
         });
 
         this.newWindowMarkup.querySelector('.fa-window-restore').addEventListener('click', function () {
             handlers.restoreHandler(thisWindow);
+            console.log(thisWindow.state);
         });
         this.newWindowMarkup.querySelector('.fa-window-close').addEventListener('click', function () {
             handlers.closeHandler(thisWindow);
+            console.log(thisWindow.state);
+
         });
         this.newWindowMarkup.addEventListener('mousedown', dragMouseDown);
+        this.newWindowMarkup.addEventListener('click', function () {
+            objects.forEach(function (items) {
+                items.zIndex = 0;
+            })
+            document.querySelectorAll('.windows').forEach(
+                function (divs) {
+                    divs.style.zIndex = 0;
+                }
+            )
+            thisWindow.zIndex = 1;
+            this.style.zIndex = 1;
+            console.log(this.newWindowMarkup);
+        });
+
         $('.desktop').append(this.newWindowMarkup);
+
+        if (this.state == "minimized") {
+            this.newWindowMarkup.querySelector("#content").classList.add('hidden');
+        }
+        else {
+            this.newWindowMarkup.querySelector("#content").classList.remove('hidden');
+        }
 
     }
 
@@ -193,6 +256,7 @@ handlers = new function () {
     this.restoreHandler = function (win) {
         win.newWindowMarkup.querySelector('.fa-window-maximize').style.display = "inline";
         win.newWindowMarkup.querySelector('.fa-window-restore').style.display = "none";
+        win.newWindowMarkup.querySelector('#content').style.display = "block";
         win.restore();
         this.stateHandler(win, 'restored');
     }
@@ -202,4 +266,3 @@ handlers = new function () {
     }
 }
 
-bootLoader();
